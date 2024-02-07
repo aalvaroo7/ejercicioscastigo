ea165ca4eb05f42cdfe4b6d2ddb9c3b4411f88c7
aalvaroo7 <almarrom@myuax.com>
2024-02-07 18:15:28 +0100

    h

---
+++ MaximosEnteros.java
@@ -0,0 +1,15 @@
+public class MaximosEnteros {
+    public static void main(String[] args) {
+        // Mayor número entero representable por un char
+        char maxChar = Character.MAX_VALUE;
+        System.out.println("Mayor número entero (char): " + (int) maxChar);
+
+        // Mayor número entero representable por un short
+        short maxShort = Short.MAX_VALUE;
+        System.out.println("Mayor número entero (short): " + maxShort);
+
+        // Mayor número entero representable por un int
+        int maxInt = Integer.MAX_VALUE;
+        System.out.println("Mayor número entero (int): " + maxInt);
+    }
+}
